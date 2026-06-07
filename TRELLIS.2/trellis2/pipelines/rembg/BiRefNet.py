5176550701e76aca8a007aef6b240d3c5d1cd88e
vdeaugustine <vincentd543@gmail.com>
2026-06-07 01:19:26 -0700

    Merge TRELLIS.2 subdirectory into main repository

---
+++ TRELLIS.2/trellis2/pipelines/rembg/BiRefNet.py
@@ -0,0 +1,47 @@
+from typing import *
+from transformers import AutoModelForImageSegmentation
+import torch
+from torchvision import transforms
+from PIL import Image
+
+
+class BiRefNet:
+    def __init__(self, model_name: str = "ZhengPeng7/BiRefNet"):
+        self.model = AutoModelForImageSegmentation.from_pretrained(
+            model_name, trust_remote_code=True
+        )
+        self.model.eval()
+        self.transform_image = transforms.Compose(
+            [
+                transforms.Resize((1024, 1024)),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]
+        )
+    
+    @property
+    def device(self):
+        return next(self.model.parameters()).device
+
+    def to(self, device):
+        self.model.to(device)
+        return self
+
+    def cuda(self):
+        self.model.to(self.device)
+
+    def cpu(self):
+        self.model.cpu()
+        
+    def __call__(self, image: Image.Image) -> Image.Image:
+        image_size = image.size
+        input_images = self.transform_image(image).unsqueeze(0).to(self.device)
+        # Prediction
+        with torch.no_grad():
+            preds = self.model(input_images)[-1].sigmoid().cpu()
+        pred = preds[0].squeeze()
+        pred_pil = transforms.ToPILImage()(pred)
+        mask = pred_pil.resize(image_size)
+        image.putalpha(mask)
+        return image
+    
